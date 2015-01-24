855aa7af97f930fe059ea5a78ba26e701f7ff42b
fBrx <admin@florian-m.net>
2015-01-24 20:12:22 +0100

    added inital version of elk image

---
+++ elk/docker_entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#start nginx
+service nginx start
+
+#start elasticsearch
+elasticsearch-1.4.2/bin/elasticsearch > /var/log/elasticsearch.log &
+
+#start logstash
+logstash-1.4.2/bin/logstash -f logstash-1.4.2/logstash.conf > /var/log/logstash.log &
