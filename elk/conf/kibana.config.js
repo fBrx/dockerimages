fb692ab0c84c34b511cdb19d366210959a0f0f7f
fBrx <admin@florian-m.net>
2015-01-28 18:00:10 +0100

    added ssl config to nginx

--- elk/conf/kibana.config.js
+++ elk/conf/kibana.config.js
@@ -29,7 +29,7 @@ function (Settings) {
      *  +elasticsearch: {server: "http://localhost:9200", withCredentials: true}+
      *
      */
-    elasticsearch: "http://"+window.location.hostname+":"+window.location.port+"/elasticsearch",
+    elasticsearch: "//"+window.location.hostname+":"+window.location.port+"/elasticsearch",
 
     /** @scratch /configuration/config.js/5
      *
