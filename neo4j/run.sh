fb5b2700b4f960b7a20207420c10848a63a8b516
fBrx <admin@florian-m.net>
2015-04-22 00:49:18 +0200

    added neo4j

---
+++ neo4j/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+NEO4J_HOME=/var/lib/neo4j
+
+sed -i "s|#org.neo4j.server.webserver.address=0.0.0.0|org.neo4j.server.webserver.address=$HOSTNAME|g" $NEO4J_HOME/conf/neo4j-server.properties
+
+# (http://neo4j.com/docs/1.6.2/configuration-linux-notes.html#_setting_the_number_of_open_files)
+limit=`ulimit -n`
+if [ "$limit" -lt 65536 ]; then
+    ulimit -n 65536;
+fi
+
+$NEO4J_HOME/bin/neo4j console
